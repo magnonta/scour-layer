ad43831a02598f5fc5b024688a001542744dec9a
Magno Oliveira dos Santos <magno.santos@conductor.com.br>
2018-10-29 09:24:39 -0300

    add variables

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	getopt "github.com/pborman/getopt/v2"
+	socketio "github.com/googollee/go-socket.io"
+)	
+
+//define MAX_PING 30
+
+func erro(*msg) const{
+    log.Fatal(msg)
+    exit(0)
+}
+	
+}
+
+func msecs (){
+	//Escrever uma função
+	//que dê a diferença de tempo em milisegundos
+}
+ 
+
+func main(argc int, *argv[] string)  {
+
+	cont ::= 0
+    p := 0 int
+    teste[30] float64
+    mev, mav, med, dp, soma float64
+    sockfd := 0, portno, n int
+    sockaddr_in serv_addr struct
+    hostent *server struct
+    timeval http_time_snd struct
+	timeval http_time_rec struct
+	pagina[256] := "/" string
+	c, i, zabbix, bytes := 1, print_buffer := 0 int
+	HTTPD_200[] := "HTTP/1.1 200"
+	HTTPD_301[] := "HTTP/1.1 301"
+	HTTPD_400[] := "HTTP/1.1 400"
+	HTTPD_404[] := "HTTP/1.1 404"
+	HTTPD_408[] := "HTTP/1.1 408"
+	HTTPD_500[] := "HTTP/1.1 500"
+	HTTPD_502[] := "HTTP/1.1 502"
+  	HTTPD_503[] := "HTTP/1.1 503"
+	HTTPD_504[] := "HTTP/1.1 504"
+	buffer[256] string
+    bufferRec[256] string
+	httpmsg[256] string
+ 	httpserver[256] string
+ 	cont := 5 
+ 	zabbix := 0 
+ 	portno := 80 
+
+	 for ((c = getopt(argc, argv, "h:p:c:w:z:b")) != EOF){
+		switch (c)
+		{
+			case 'h':
+				strcpy(httpserver,optarg);
+				break;
+			case 'p':
+				portno = atoi(optarg);
+				break;	
+			case 'c':
+				cont = atoi(optarg);
+				if(cont > MAX_PING){
+				printf("Number of pings Maximum = 30 \n");
+				}
+				break;
+			case 'w':
+				strcpy(pagina,optarg);
+				break;
+			case 'z':
+				zabbix = 1;
+				break;
+			case 'b':
+				print_buffer=1;
+				break;
+			default :
+				printf("USAGE: HTTPPING \n\t-h Hostname \n\t-p Port \n\t-c Number of pings Maximum 30 \n\t-w Enter the Page \n\t-z Exit to the Zabbix  \n\t-b Print server Response\n ");
+				
+			return 0;	 
+	
+
+	return 0
+}
+
