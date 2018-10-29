18cb4887972939b5bf54770204561a63ea3bd21b
Magno Oliveira dos Santos <magno.santos@conductor.com.br>
2018-10-29 17:18:44 -0300

    ajuste de identação

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ import (
 //define MAX_PING 30
 
 func erro(*msg) const{
-    log.Fatal(msg)
-    exit(0)
+	log.Fatal(msg)
+	exit(0)
 }
 	
 }
@@ -26,13 +26,13 @@ func msecs (){
 func main(argc int, *argv[] string)  {
 
 	cont ::= 0
-    p := 0 int
-    teste[30] float64
-    mev, mav, med, dp, soma float64
-    sockfd := 0, portno, n int
-    sockaddr_in serv_addr struct
-    hostent *server struct
-    timeval http_time_snd struct
+	p := 0 int
+	teste[30] float64
+	mev, mav, med, dp, soma float64
+	sockfd := 0, portno, n int
+	sockaddr_in serv_addr struct
+	hostent *server struct
+	timeval http_time_snd struct
 	timeval http_time_rec struct
 	pagina[256] := "/" string
 	c, i, zabbix, bytes := 1, print_buffer := 0 int
@@ -43,45 +43,45 @@ func main(argc int, *argv[] string)  {
 	HTTPD_408[] := "HTTP/1.1 408"
 	HTTPD_500[] := "HTTP/1.1 500"
 	HTTPD_502[] := "HTTP/1.1 502"
-  	HTTPD_503[] := "HTTP/1.1 503"
+	HTTPD_503[] := "HTTP/1.1 503"
 	HTTPD_504[] := "HTTP/1.1 504"
 	buffer[256] string
-    bufferRec[256] string
+	bufferRec[256] string
 	httpmsg[256] string
- 	httpserver[256] string
- 	cont := 5 
- 	zabbix := 0 
- 	portno := 80 
+	httpserver[256] string
+	cont := 5 
+	zabbix := 0 
+	portno := 80 
 
-	 for ((c = getopt(argc, argv, "h:p:c:w:z:b")) != EOF){
-		switch (c)
-		{
-			case 'h':
-				strcpy(httpserver,optarg);
-				break;
-			case 'p':
-				portno = atoi(optarg);
-				break;	
-			case 'c':
-				cont = atoi(optarg);
-				if(cont > MAX_PING){
-				printf("Number of pings Maximum = 30 \n");
-				}
-				break;
-			case 'w':
-				strcpy(pagina,optarg);
-				break;
-			case 'z':
-				zabbix = 1;
-				break;
-			case 'b':
-				print_buffer=1;
-				break;
-			default :
-				printf("USAGE: HTTPPING \n\t-h Hostname \n\t-p Port \n\t-c Number of pings Maximum 30 \n\t-w Enter the Page \n\t-z Exit to the Zabbix  \n\t-b Print server Response\n ");
-				
-			return 0;	 
-	
+	// for ((c = getopt(argc, argv, "h:p:c:w:z:b")) != EOF){
+	// switch (c)
+	// {
+	// 	case 'h':
+	// 		strcpy(httpserver,optarg);
+	// 		break;
+	// 	case 'p':
+	// 		portno = atoi(optarg);
+	// 		break;	
+	// 	case 'c':
+	// 		cont = atoi(optarg);
+	// 		if(cont > MAX_PING){
+	// 		printf("Number of pings Maximum = 30 \n");
+	// 		}
+	// 		break;
+	// 	case 'w':
+	// 		strcpy(pagina,optarg);
+	// 		break;
+	// 	case 'z':
+	// 		zabbix = 1;
+	// 		break;
+	// 	case 'b':
+	// 		print_buffer=1;
+	// 		break;
+	// 	default :
+	// 		printf("USAGE: HTTPPING \n\t-h Hostname \n\t-p Port \n\t-c Number of pings Maximum 30 \n\t-w Enter the Page \n\t-z Exit to the Zabbix  \n\t-b Print server Response\n ");
+			
+	// 	return 0;	 
+
 
 	return 0
 }
