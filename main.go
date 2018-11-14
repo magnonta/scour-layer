30c7fd726122424d65791bd0d1d7baae1d7a01e7
Magno Oliveira <moliveira@localhost.localdomain>
2018-11-13 22:10:03 -0300

    httping go

--- main.go
+++ main.go
@@ -2,87 +2,72 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	getopt "github.com/pborman/getopt/v2"
-	socketio "github.com/googollee/go-socket.io"
-)	
+	"os"
+	"strconv"
+	"time"
+)
 
-//define MAX_PING 30
+func check(url, port string) (urltime float64, urlsize int) {
+	t0 := time.Now()
+	client := &http.Client{}
 
-func erro(*msg) const{
-	log.Fatal(msg)
-	exit(0)
-}
-	
-}
+	// default case: HTTP request
+	domain := "http://" + url + ":" + port
+	switch port {
+	case "80":
+		domain = "http://" + url
+	case "443":
 
-func msecs (){
-	//Escrever uma função
-	//que dê a diferença de tempo em milisegundos
-}
- 
+		domain = "https://" + url
+	}
 
-func main(argc int, *argv[] string)  {
+	req, err := http.NewRequest("GET", domain, nil)
+	if err != nil {
+		// handle error
+		log.Fatalf("|----- Não foi possível conectar %s", url)
+	} else {
+		req.Proto = "HTTP/1.1"
+		req.ProtoMinor = 0
+		req.Header.Set("User-Agent", "GoLang httping v0.1")
 
-	cont ::= 0
-	p := 0 int
-	teste[30] float64
-	mev, mav, med, dp, soma float64
-	sockfd := 0, portno, n int
-	sockaddr_in serv_addr struct
-	hostent *server struct
-	timeval http_time_snd struct
-	timeval http_time_rec struct
-	pagina[256] := "/" string
-	c, i, zabbix, bytes := 1, print_buffer := 0 int
-	HTTPD_200[] := "HTTP/1.1 200"
-	HTTPD_301[] := "HTTP/1.1 301"
-	HTTPD_400[] := "HTTP/1.1 400"
-	HTTPD_404[] := "HTTP/1.1 404"
-	HTTPD_408[] := "HTTP/1.1 408"
-	HTTPD_500[] := "HTTP/1.1 500"
-	HTTPD_502[] := "HTTP/1.1 502"
-	HTTPD_503[] := "HTTP/1.1 503"
-	HTTPD_504[] := "HTTP/1.1 504"
-	buffer[256] string
-	bufferRec[256] string
-	httpmsg[256] string
-	httpserver[256] string
-	cont := 5 
-	zabbix := 0 
-	portno := 80 
+		resp, err := client.Do(req)
+		if err != nil {
+			// handle error
+			log.Fatalf("|----- Não foi possível conectar %s\n", url)
+		} else {
+			defer resp.Body.Close()
+			body, _ := ioutil.ReadAll(resp.Body)
 
-	// for ((c = getopt(argc, argv, "h:p:c:w:z:b")) != EOF){
-	// switch (c)
-	// {
-	// 	case 'h':
-	// 		strcpy(httpserver,optarg);
-	// 		break;
-	// 	case 'p':
-	// 		portno = atoi(optarg);
-	// 		break;	
-	// 	case 'c':
-	// 		cont = atoi(optarg);
-	// 		if(cont > MAX_PING){
-	// 		printf("Number of pings Maximum = 30 \n");
-	// 		}
-	// 		break;
-	// 	case 'w':
-	// 		strcpy(pagina,optarg);
-	// 		break;
-	// 	case 'z':
-	// 		zabbix = 1;
-	// 		break;
-	// 	case 'b':
-	// 		print_buffer=1;
-	// 		break;
-	// 	default :
-	// 		printf("USAGE: HTTPPING \n\t-h Hostname \n\t-p Port \n\t-c Number of pings Maximum 30 \n\t-w Enter the Page \n\t-z Exit to the Zabbix  \n\t-b Print server Response\n ");
-			
-	// 	return 0;	 
+			url_size := len(body)
+			//fmt.Printf("%s", body)
+			msec := time.Since(t0)
+			url_time := msec.Seconds() * float64(time.Second/time.Millisecond)
 
-
-	return 0
+			return url_time, url_size
+		}
+	}
+	return
 }
 
+func main() {
+	fmt.Printf("GoLang httping - PINGING %s\n", os.Args[1])
+
+	port := "80"
+	// Do we have port defined ?
+	if os.Args[2] != "" {
+		port = os.Args[2]
+	}
+
+	seq := 0
+	// infinite loop
+	for {
+		seq = seq + 1
+		t, s := check(os.Args[1], port)
+		fmt.Printf("pingando %s:%s, seq=%d time=%s bytes=%d\n", os.Args[1], port, seq, strconv.FormatFloat(t, 'f', 3, 64), s)
+		time.Sleep(1 * time.Second)
+	}
+
+}
